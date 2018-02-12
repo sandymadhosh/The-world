cf1a9e4b51ed5c7a258601bf9b53874f97fc2f38
sandymadhosh <35587680+sandymadhosh@users.noreply.github.com>
2018-02-12 16:31:05 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,61 @@
+#types of data types
+
+#1. numbers - int, floating, complex:
+
+complex(2,3)
+#output=(2+3j)
+complex(5,-6)
+#output=(5-6j)
+int(30-50)
+#output=-20
+int("111"+"222")
+#output=111222
+float(99.5-66.7)
+#output=32.8
+float(66.9/55.3)
+#output=1.2097649186256783
+
+#2. lists
+L=[1,2,3,"HI",5]
+L[4]
+L[3]
+#output=5
+#output2='HI'
+
+#3.tuples
+tup1 = ('physics', 'chemistry', 1997,2000)
+tup2 = (1,2,3,4,5,6,7)
+tup1[0]
+#output='physics'
+tup2[1:5]
+#output=(2, 3, 4, 5)
+
+#4.dictoinary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Name']
+#output='Zara'
+dict['Age']
+#output=7
+
+#5.string
+var1 = 'Hello World!'
+var2 = "Python Programming"
+var1[0]
+#output='H'
+var2[1:5]
+#output='ytho'
+
+#6.sets
+set={1,2,2,3,4,5}
+set2={3,11,5,6,7,}
+set
+#output={1, 2, 3, 4, 5}
+
+set={1,1,1,2,3,4,5,6}
+set2={4,5,6,7,8}
+set
+#output{1, 2, 3, 4, 5, 6}
+
+#7. boolian
+2*3==3+3
+#output=true
