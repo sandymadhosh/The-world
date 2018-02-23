af6aa9fe15ab6f9beff885088ebf99b9fa882aff
sandymadhosh <35587680+sandymadhosh@users.noreply.github.com>
2018-02-23 10:22:05 +0530

    Add files via upload

---
+++ brkandcont.py
@@ -0,0 +1,46 @@
+#break and continue
+print("using break")
+for i in[1,2,3,4,5]:
+    if (i==3):
+        break
+    print(i)
+
+print("out of the loop")
+print(i)
+    
+
+print("using continue")
+
+for i in[1,2,3,4,5]:
+    if (i==3):
+        continue
+    print(i)    
+
+print("using range")
+for i in range(10):
+    print (i)
+
+print("using range part 2")
+for i in range(50,101):
+     print(i)
+
+
+print ("using range part 3")
+for i in range (2,20,2):
+      print(i)
+      
+print ("using range part 4")
+for i in range (0,11,2):
+    print(i)
+
+print ("using range part 5")
+for i in range (1,12,3):
+     print (i)
+
+
+    
+
+
+ 
+
+     
---
+++ func.py
@@ -0,0 +1,40 @@
+#function
+
+    
+
+def add(x,y):
+    print("addition")
+    return x+y
+
+
+   
+
+def sub(x,y):
+    print("subraction")
+    return x-y
+
+
+
+def multiply(x,y):
+    print("multiplication")
+    return x*y
+
+
+ 
+
+def divide(x,y):
+    print("division")
+    return x/y
+
+
+def floordivision(x,y):
+    print("flo")
+    return x//y
+
+
+print((add (9,7)))
+print((sub(5,7)))
+print((multiply(20,10)))
+print((divide(20,2)))
+print((floordivision(60,10)))
+
